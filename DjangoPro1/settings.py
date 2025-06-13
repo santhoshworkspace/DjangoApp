fcc9d3ebf561a4891f1f28f9356f4ff0071eec02
santhoshworkspace.gmail.com <santhoshworkspace@gmail.com>
2025-06-13 07:29:15 +0530

    created register data

--- DjangoPro1/settings.py
+++ DjangoPro1/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DjangoPro'
+    'DjangoPro',
+    'users'
 ]
 
 MIDDLEWARE = [
--- DjangoPro1/urls.py
+++ DjangoPro1/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('DjangoPro/',include('DjangoPro.urls'))
+    path('DjangoPro/',include('DjangoPro.urls')),
+    path('register/',include('users.urls'))
 ]
