bf89ef24e2671da10b42b89c6531dee731b6cc63
santhoshworkspace.gmail.com <santhoshworkspace@gmail.com>
2025-06-14 10:49:11 +0530

    logout added

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.register,name='register'),
     path('login/',auth_views.LoginView.as_view(template_name='Login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='Logout.html'),name='logout')
+    path('logout/',auth_views.LogoutView.as_view(next_page='/login/'),name='logout')
 ]
